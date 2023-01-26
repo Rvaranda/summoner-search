17f3c004fb45b7fcc3d5ad065f7d60f7b523f9c1
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-01-26 20:53:50 -0300

    Improved matches list

--- src/components/Match.js
+++ src/components/Match.js
@@ -7,42 +7,58 @@ import './Match.css';
 
 function Match({summonerId, champions}) {
     const [matchIds, setMatchIds] = useState([]);
+    // const matchesData = useMatch(summonerId, 0, 3);
     const [matchesData, setMatchesData] = useState(null);
 
     useEffect(() => {
-        regionalEndpoint.get(`/lol/match/v5/matches/by-puuid/${summonerId}/ids?start=0&count=10`)
+        regionalEndpoint.get(`/lol/match/v5/matches/by-puuid/${summonerId}/ids?start=0&count=3`)
+        .then(response => Promise.all(response.data.map(matchId => regionalEndpoint.get(`/lol/match/v5/matches/${matchId}`))))
         .then(response => {
-            setMatchIds(response.data);
-            return Promise.all(response.data.map(matchId => regionalEndpoint.get(`/lol/match/v5/matches/${matchId}`)));
-        }).then(response => {
-            console.log(response.map(e => e.data));
-            setMatchesData(response.map(e => e.data))
+            const summonerMatches = [];
+            const matches = response.map(e => e.data);
+            matches.forEach(match => match.info.participants.forEach((part, index) => {
+                if (summonerId === part.puuid) summonerMatches.push({summonerIndex: index, match: match});
+            }));
+            setMatchesData(summonerMatches);
         });
-    },[]);
-
-    console.log('teste');
+    }, []);
 
     return (
         <>
         { matchesData && matchesData.map(match => {
             return (
-                <div key={match.metadata.matchId} className={`match ${match.info.participants[2].win ? 'victory' : 'defeat'}`}>
+                <div key={match.match.metadata.matchId} className={`match ${match.match.info.participants[match.summonerIndex].win ? 'victory' : 'defeat'}`}>
                     <img
-                        src={`${champIconsUrl}/${champions[match.info.participants[2].championId].image.full}`}
+                        src={`${champIconsUrl}/${champions[match.match.info.participants[match.summonerIndex].championId].image.full}`}
                         alt='champion'
                     />
                     <div className='match-items'>
-                        <img src={`${itemIconUrl}/${match.info.participants[2].item0}.png`} alt="item"/>
-                        <img src={`${itemIconUrl}/${match.info.participants[2].item1}.png`} alt="item"/>
-                        <img src={`${itemIconUrl}/${match.info.participants[2].item2}.png`} alt="item"/>
-                        <img src={`${itemIconUrl}/${match.info.participants[2].item3}.png`} alt="item"/>
-                        <img src={`${itemIconUrl}/${match.info.participants[2].item4}.png`} alt="item"/>
-                        <img src={`${itemIconUrl}/${match.info.participants[2].item6}.png`} alt="item"/>
+                        {match.match.info.participants[match.summonerIndex].item0 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item0}.png`} alt="item"/>}
+                        {match.match.info.participants[match.summonerIndex].item1 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item1}.png`} alt="item"/>}
+                        {match.match.info.participants[match.summonerIndex].item2 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item2}.png`} alt="item"/>}
+                        {match.match.info.participants[match.summonerIndex].item3 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item3}.png`} alt="item"/>}
+                        {match.match.info.participants[match.summonerIndex].item4 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item4}.png`} alt="item"/>}
+                        {match.match.info.participants[match.summonerIndex].item5 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item5}.png`} alt="item"/>}
+                        {match.match.info.participants[match.summonerIndex].item6 === 0 ? 
+                            <div style={{display: 'inline-block', width: '40px', height: '40px', backgroundColor: '#050505'}}></div> :
+                            <img src={`${itemIconUrl}/${match.match.info.participants[match.summonerIndex].item6}.png`} alt="item"/>}
                     </div>
-                    <p className='match-result'>{match.info.participants[2].win ? 'Vitória' : 'Derrota'}</p>
+                    <p className='match-result'>{match.match.info.participants[match.summonerIndex].win ? 'Vitória' : 'Derrota'}</p>
                     <div className='match-info'>
-                        <p>12/07/15</p>
-                        <p>33:07</p>
+                        <p>{`${new Date(match.match.info.gameCreation).getDate()}/${new Date(match.match.info.gameCreation).getMonth()}/${new Date(match.match.info.gameCreation).getFullYear()}`}</p>
+                        <p>{`${new Date(match.match.info.gameDuration * 1000).getMinutes()}:${new Date(match.match.info.gameDuration * 1000).getSeconds()}`}</p>
                     </div>
                 </div>
             );
--- src/config.js
+++ src/config.js
@@ -5,16 +5,18 @@ export const champIconsUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/img/
 export const profileIconUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/img/profileicon';
 export const itemIconUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/img/item';
 
+const api = 'RGAPI-18a3ef3b-80e2-4639-bb65-946bc65b6db4';
+
 export const endpoint = axios.create({
     baseURL: 'https://br1.api.riotgames.com',
     params: {
-        api_key: 'RGAPI-dfa73ecb-5d63-420c-b4ce-14593b4abbb2'
+        api_key: api
     }
 });
 
 export const regionalEndpoint = axios.create({
     baseURL: 'https://americas.api.riotgames.com',
     params: {
-        api_key: 'RGAPI-dfa73ecb-5d63-420c-b4ce-14593b4abbb2'
+        api_key: api
     }
 });
