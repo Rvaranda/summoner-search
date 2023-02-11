b4289b2164a8d2541e0ccc04bf82dd5178adf74a
Rvaranda <rafael_andrademiranda@yahoo.com.br>
2023-02-11 09:17:13 -0300

    Changed protocol to https

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-export const champUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/data/pt_BR/champion';
-export const champIconsUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion';
-export const profileIconUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/img/profileicon';
-export const itemIconUrl = 'http://ddragon.leagueoflegends.com/cdn/13.1.1/img/item';
+export const champUrl = 'https://ddragon.leagueoflegends.com/cdn/13.1.1/data/pt_BR/champion';
+export const champIconsUrl = 'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/champion';
+export const profileIconUrl = 'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/profileicon';
+export const itemIconUrl = 'https://ddragon.leagueoflegends.com/cdn/13.1.1/img/item';
 
 export const endpoint = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://summoner-search.herokuapp.com',
 });
